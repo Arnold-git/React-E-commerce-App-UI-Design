29a9133f7b849a2a4f8110022b63a665d381f82d
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-09-19 18:51:34 +0100

    components :rockets:

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Search } from '@material-ui/icons';
 import React from 'react'
 import styled from 'styled-components'
 
@@ -8,17 +9,45 @@ const Container = styled.div`
 
 const Wrapper = styled.div`
     padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 `;
 
 const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
 
 `
+
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer; 
+`
+const SearchContainer = styled.div`
+    border: 1px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+`;
+
+const Input = styled.input`
+    border: none
+`;
+
 const Center = styled.div`
+flex: 1;
+`;
 
+const Logo = styled.h1`
+    font-weight: bold
 `
+
 const Right = styled.div`
-
-`
+flex: 1;
+`;
     
 
 
@@ -26,8 +55,14 @@ function Navbar() {
     return (
         <Container>
             <Wrapper>
-                <Left>Left</Left>
-                <Center>Right</Center>
+                <Left>
+                    <Language>EN</Language>
+                    <SearchContainer>
+                        <Input/>
+                        <Search/>
+                    </SearchContainer>
+                </Left>
+                <Center><Logo>ARNOLD.</Logo></Center>
                 <Right>Right</Right>
             </Wrapper>
         </Container>
