6a641772963c3a422c8a5a502f7dad84961ff6e6
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-09-20 14:39:18 +0100

    :rockets:

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -6,6 +6,7 @@ const Container = styled.div`
     height: 100vh;
     display: flex;
     position: relative;
+    overflow: hidden;
 `
 const Arrow = styled.div`
     width: 50px;
@@ -23,12 +24,15 @@ const Arrow = styled.div`
     margin: auto;
     cursor: pointer;
     opacity: 0.5;
+    z-index: 2;
 
 `;
 
 
 const Wrapper = styled.div`
-    height: 100%
+    height: 100%;
+    display: flex;
+    transform: translateX(0vw)
 `
 const Slide = styled.div`
     display: flex;
@@ -48,7 +52,7 @@ const Image = styled.img`
 
 const InfoContainer = styled.div`
     flex: 1;
-    padding: 50px;
+    padding: 500px;
 `;
 
 const Title = styled.h1`
@@ -58,10 +62,14 @@ const Desc = styled.p`
     margin: 50px 0;
     font-size: 20px;
     font-weight: 500;
+    letter-spacing: 1px;
 
 `
 const Button = styled.button`
-
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
 `
 
 
@@ -73,12 +81,32 @@ const Slider = () => {
                 <ArrowLeftOutlined/>
             </Arrow>
             <Wrapper>
-                <Slide>
+                <Slide bg="f5fafd">
                 <ImgContainer>
                     <Image src="https://i.ibb.co/XsdmR2c/1.png" />
                 </ImgContainer>
                 <InfoContainer>
-                    <Title>FLASH SALES</Title>
+                    <Title>SUMMER SALES</Title>
+                    <Desc>DONT COMPROMISE ON STYLE GET 30% OFF FOR NEW ARRIVAL</Desc>
+                    <Button>SHOP NOW</Button>
+                </InfoContainer>
+                </Slide>
+                <Slide bg="f5fafd">
+                <ImgContainer>
+                    <Image src="https://i.ibb.co/XsdmR2c/1.png" />
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>WINTER SALES</Title>
+                    <Desc> DONT COMPROMISE ON STYLE GET 30% OFF FOR NEW ARRIVAL </Desc>
+                    <Button>SHOP NOW</Button>
+                </InfoContainer>
+                </Slide>
+                <Slide bg="f5fafd">
+                <ImgContainer>
+                    <Image src="https://i.ibb.co/XsdmR2c/1.png" />
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>SALES</Title>
                     <Desc>DONT COMPROMISE ON STYLE GET 30% OFF FOR NEW ARRIVAL</Desc>
                     <Button>SHOP NOW</Button>
                 </InfoContainer>
