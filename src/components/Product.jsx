6e3189ad2f0c4aab9ec85efe0a3ca6f00b808c34
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-09-22 10:20:47 +0100

    some fixes

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -4,8 +4,18 @@ import styled from "styled-components"
 
 
 const Container = styled.div`
+    flex:1;
+    margin: 5px;
 
 `
+
+const Circle = styled.div`
+
+`
+const Image = styled.img``
+    
+const Info = styled.div``
+const Icon = styled.div`` 
 
 const Product = ({item}) => {
     return (
