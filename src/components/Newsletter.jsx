8e7bb5c7c6498db1f6e6e2d88d5edd45efa06395
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-09-23 11:00:55 +0100

    newletter :fire:

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,12 +2,53 @@ import { Send } from "@material-ui/icons";
 import styled from "styled-components";
 
 
-const Container = styled.div`` 
-const Title = styled.h1`` 
-const Desc = styled.div`` 
-const InputContainer = styled.div`` 
-const Input = styled.input`` 
-const Button = styled.button`` 
+const Container = styled.div`
+    height: 60vh;
+    background-color: #fcf5f5;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+` 
+const Title = styled.h1`
+    font-size: 70px;
+    margin-bottom: 20px;
+
+` 
+
+
+const Desc = styled.div`
+    font-size: 24px;
+    font-weight: 300;
+    margin-bottom: 20px;
+
+`; 
+
+
+const InputContainer = styled.div`
+    width: 50%;
+    height: 40px;
+    background-color: white;
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid lightgray;
+
+
+` 
+const Input = styled.input`
+    border: none;
+    flex: 8;
+    padding-left: 20px;
+`
+
+
+const Button = styled.button`
+    flex: 1;
+    border: none;
+    background-color: teak;
+    color: white;
+` 
 
 
 
@@ -15,10 +56,10 @@ const Button = styled.button``
 const Newsletter = () => {
     return (
         <Container>
-            <Title></Title>
-            <Desc></Desc>
+            <Title>Newletter </Title>
+            <Desc>Get timely update for your favourite products</Desc>
             <InputContainer>
-                <Input/>
+                <Input placeholder="Input your email"/>
                 <Button>
                     <Send />
                 </Button>
