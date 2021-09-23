888aa201932e964efd56a9a8865b67d8ce9def67
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-09-23 10:34:18 +0100

    newletter :fire:

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,12 +1,22 @@
-import { Button, Input } from '@material-ui/core'
-import { Description, Title } from '@material-ui/icons'
-import React from 'react'
+import { Send } from "@material-ui/icons";
+import styled from "styled-components";
+
+
+const Container = styled.div`` 
+const Title = styled.h1`` 
+const Desc = styled.div`` 
+const InputContainer = styled.div`` 
+const Input = styled.input`` 
+const Button = styled.button`` 
+
+
+
 
 const Newsletter = () => {
     return (
         <Container>
             <Title></Title>
-            <Description></Description>
+            <Desc></Desc>
             <InputContainer>
                 <Input/>
                 <Button>
