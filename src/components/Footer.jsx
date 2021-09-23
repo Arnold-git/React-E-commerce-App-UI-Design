06141351523c6f3026bc59fd6f488b9a0699e7a7
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-09-23 11:44:36 +0100

    :fire:

---
+++ src/components/Footer.jsx
@@ -0,0 +1,60 @@
+import { Facebook, Instagram, LinkedIn, Twitter } from "@material-ui/icons"
+import styled from "styled-components"
+
+
+const Container = styled.div`
+    display: flex;
+`
+
+const Left = styled.div`
+    flex: 1;
+`
+const Logo = styled.div`
+    flex: 1;
+`
+const Desc = styled.p`
+    flex: 1;
+`
+
+const Center = styled.div`
+    flex: 1;
+`
+const Logo = styled.div`
+    flex: 1;
+`
+
+const Right = styled.div`
+    flex: 1;
+`
+
+
+const Footer = () => {
+    return (
+        <Container>
+            <Left>
+                <Logo>ARNOLD.DEV</Logo>
+                <Desc>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem repellendus eveniet vitae voluptates excepturi culpa, aliquam illo soluta asperiores, quos blanditiis ullam quis atque similique distinctio id minima architecto! Similique.
+                </Desc>
+                <SocialContainer>
+                    <SocialIcon>
+                        <Facebook/>
+                    </SocialIcon>
+                    <SocialIcon>
+                        <Instagram/>
+                    </SocialIcon>
+                    <SocialIcon>
+                        <Twitter/>
+                    </SocialIcon>
+                    <SocialIcon>
+                        <LinkedIn/>
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
+            <Center></Center>
+            <Right></Right>
+        </Container>
+    )
+}
+
+export default Footer
