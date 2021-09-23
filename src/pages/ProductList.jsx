c3ed488f6a28ab672dd616f4fd8fa6f95b9b454c
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-09-23 21:54:32 +0100

    product list initial commit :fire:

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -6,7 +6,19 @@ import Navbar from "../components/Navbar"
 
 
 const Container = styled.div``
+const Title = styled.h1`
+    margin: 20px;
+`
 
+
+const FilterContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+
+`
+const Filter = styled.div`
+    margin: 20px;
+`
 
 
 const ProductList = () => {
