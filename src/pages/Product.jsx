6a1edbe1c32e305ce7cba687a57ecbeb517bb9f8
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-09-24 12:30:04 +0100

    :fire:

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -18,24 +18,27 @@ const Image = styled.img`
     width: 100%;
     height: 90vh;
     object-fit: cover;
-`
+`;
 
 
 const InfoContainer = styled.div`
     flex: 1;
     padding: 0px 50px;
-`
+`;
 
 const Title = styled.h1`
     font-weight: 200;
-`
+`;
 
 const Desc = styled.p`
     margin: 20px 0px;
-    
-`
 
-const Price = styled.span``
+`;
+
+const Price = styled.span`
+    font-weight: 100;
+    font-size: 40px;
+`;
 
 
 
