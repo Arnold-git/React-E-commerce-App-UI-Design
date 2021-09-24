faa0a87ad3130d74e81520efb8e4ec81218bda07
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-09-24 14:16:20 +0100

    :fire:

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -95,6 +95,10 @@ const Amount = styled.span`
     height: 30px;
     border-radius: 10px;
     border: 1px solid teal;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 5px;
 `;
 
 const Button = styled.div`
