a916a69ce006ee93d16e27c4a8444fc24f9add53
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-09-24 12:20:26 +0100

    :fire:

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,8 +3,18 @@ import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
 import Newsletter from "../components/Newsletter"
 import Annoucement from "../components/Annoucement"
+import { Title } from "@material-ui/icons"
 
 const Container = styled.div``
+const Wrapper = styled.div``
+const ImgContainer = styled.div``
+const Image = styled.div``
+const InfoContainer = styled.div``
+
+
+
+
+
 
 const Product = () => {
     return (
@@ -13,7 +23,15 @@ const Product = () => {
             <Annoucement/>
             <Wrapper>
                 <ImgContainer>
+                    <Image src=""/>
                 </ImgContainer>
+                <InfoContainer>
+                    <Title>Best Deal youc can get</Title>
+                    <Desc>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos dolorem facere eligendi incidunt magni repellat possimus? Possimus, tempora nobis. Vero quod quo quam sed doloremque veniam qui porro provident veritatis.
+                    </Desc>
+                    <Price> $20</Price>
+                </InfoContainer>
             </Wrapper>
             <Newsletter/>
             <Footer/>
