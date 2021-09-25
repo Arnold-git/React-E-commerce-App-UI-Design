b70ba46194cf1730087e55420d1d1b958d3efcd0
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-09-25 21:30:26 +0100

    cart :rocket:

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,9 +6,31 @@ import Navbar from "../components/Navbar"
 
 
 const Container = styled.div``
-const Wrapper = styled.div``
-const Title = styled.div``
-const Top = styled.div``
+
+
+const Wrapper = styled.div`
+    padding: 20px;
+`
+
+
+const Title = styled.h1`
+    font-weight: 300;
+    text-align: center;
+`
+
+const Top = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+`
+
+const TopButton = styled.button`
+    padding: 10px;
+    font-weight: 600;
+    cursor: pointer;
+`
+
 const Botton = styled.div``
 
 
@@ -19,7 +41,10 @@ const Cart = () => {
             <Annoucement/>
             <Wrapper>
                 <Title>YOUR BAGS</Title>
-                <Top></Top>
+                <Top>
+                    <TopButton>CONTINUE SHOPPING</TopButton>
+                    <TopButton>CHECKOUT NOW</TopButton>
+                </Top>
                 <Bottom></Bottom>
             </Wrapper>
             <Footer/>    
