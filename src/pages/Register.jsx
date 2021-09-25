94352efd835b5d1a53f38120df5732de2240dac8
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-09-25 12:13:19 +0100

    register and login :fire:

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,11 +4,14 @@ const Container = styled.div`
     width: 100vw;
     height: 100vh;
     background: linear-gradient(
-        rgba(255, 255, 255, 0.5)
+        rgba(255, 255, 255, 0.5),
         rgba(255, 255, 255, 0.5)
     ),
-    url();
+    url("https://images.unsplash.com/photo-1577546568088-eb32790be7ec?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=874&q=80");
+        center;
 
+
+    background-color: cover;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -32,12 +35,39 @@ const Form = styled.form`
     flex-wrap: wrap;
 `
 
-
-
-
-
-const Input = styled.input`
+const input = styled.input`
     flex: 1;
+    min-width: 40%;
+    margin: 20px 10px 0px 0px;
+    padding: 10px;
+
+`
+
+
+const Register = () => {
+    return (
+        <Container>
+            <Wrapper >
+                <Title>CREATE AN ACCOUNT</Title>
+                <Form>
+                    <Input placeholder="name"/>
+                    <Input placeholder="last name"/>
+                    <Input placeholder="username"/>
+                    <Input placeholder="email"/>
+                    <Input placeholder="password"/>
+                    <Input placeholder="confirm password"/>
+                    <Agreement>
+                        By creating an account, I consent to the processing of my personal
+                        data in accordance with thr <b>PRIVACY POLICY</b>
+                    </Agreement>
+                    <Button>CREATE</Button>
+                </Form>
+            </Wrapper>
+        </Container>
+    )
+}
+
+export default Register
     min-width: 40%;
     margin: 20px 10px 0px 0px;
     padding: 10px;
