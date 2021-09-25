aef341efc08bc3c86129c0cd58485faf301e9e22
Arnold-git <arnold.ighiwiyisi@eng.uniben.edu>
2021-09-25 12:19:34 +0100

    register and login :fire:

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -7,7 +7,7 @@ const Container = styled.div`
         rgba(255, 255, 255, 0.5),
         rgba(255, 255, 255, 0.5)
     ),
-    url("https://images.unsplash.com/photo-1577546568088-eb32790be7ec?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=874&q=80");
+    url("https://images.unsplash.com/photo-1577546568088-eb32790be7ec?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=874&q=80")
         center;
 
 
@@ -56,53 +56,6 @@ const Button = styled.div`
     cursor: pointer;
 
 `
-
-const Register = () => {
-    return (
-        <Container>
-            <Wrapper >
-                <Title>CREATE AN ACCOUNT</Title>
-                <Form>
-                    <Input placeholder="name"/>
-                    <Input placeholder="last name"/>
-                    <Input placeholder="username"/>
-                    <Input placeholder="email"/>
-                    <Input placeholder="password"/>
-                    <Input placeholder="confirm password"/>
-                    <Agreement>
-                        By creating an account, I consent to the processing of my personal
-                        data in accordance with thr <b>PRIVACY POLICY</b>
-                    </Agreement>
-                    <Button>CREATE</Button>
-                </Form>
-            </Wrapper>
-        </Container>
-    )
-}
-
-export default Register
-    min-width: 40%;
-    margin: 20px 10px 0px 0px;
-    padding: 10px;
-`
-
-
-const Agreemet = styled.span`
-    font-size: 12px;
-    margin: 20px 0px;
-`
-
-
-const Button = styled.button`
-    width: 40%;
-    border: none;
-    padding: 15px 20px;
-    background-color: teal;
-    color: white;
-    cursor: pointer;
-`
-
-
 
 const Register = () => {
     return (
